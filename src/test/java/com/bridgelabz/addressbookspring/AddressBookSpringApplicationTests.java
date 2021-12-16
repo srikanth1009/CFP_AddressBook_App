679add6944b1755c599571f7d11cfe1d8d1c9a24
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-16 19:21:21 +0530

    Added: Intial project Structure

---
+++ src/test/java/com/bridgelabz/addressbookspring/AddressBookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
