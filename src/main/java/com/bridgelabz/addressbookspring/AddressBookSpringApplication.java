e0e9c3a2f2ea3911009b2a44b5f1827b961e7f92
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-19 12:36:00 +0530

    Added: Logging Levels using Lombok Library

--- src/main/java/com/bridgelabz/addressbookspring/AddressBookSpringApplication.java
+++ src/main/java/com/bridgelabz/addressbookspring/AddressBookSpringApplication.java
@@ -3,7 +3,10 @@ package com.bridgelabz.addressbookspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringApplication {
 
 	public static void main(String[] args) {
